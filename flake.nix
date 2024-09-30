d65368cf88caa966a4b506faf79d488b2a435b75
Kirill Kuznetsov <kdevlab@yandex.ru>
2024-09-30 15:55:02 +0300

    Add dependencies

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Home manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:miuirussia/nixpkgs?ref=nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
